c5330873ce49ee34092caeee45b796f9a7ef271c
Mateusz19337 <100776522+Mateusz19337@users.noreply.github.com>
2022-06-14 21:06:31 +0200

    Add files via upload

---
+++ Laboratorium z Flask 2/myflaskapp/app2.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import sqlite3 as sql
+app = Flask(__name__)
+@app.route("/")
+def main():
+ return render_template('index.html')
+
+@app.route("/dodaj")
+def new_student():
+ return render_template('studentadd.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+ if request.method == 'POST':
+     try:
+      imieinazwisko = request.form['imieinazwisko']
+      nrpracownika = request.form['nrpracownika']
+      adres = request.form['adres']
+
+      with sql.connect("database.db") as con:
+          cur = con.cursor()
+          cur.execute("INSERT INTO pracownicy (imieinazwisko,nrpracownika,adres) VALUES(?,?,?)",(imieinazwisko,nrpracownika,adres) )
+
+          con.commit()
+          msg = "Rekord zapisany"
+     except:
+         con.rollback()
+         msg = "Blad przy dodawaniu rekordu"
+
+     finally:
+         return render_template("rezultat.html",msg = msg)
+         con.close()
+ 
+@app.route('/lista')
+def list():
+ con = sql.connect("database.db")
+ con.row_factory = sql.Row
+ cur = con.cursor()
+ cur.execute('SELECT * FROM pracownicy ORDER BY imieinazwisko')
+ rekordy = cur.fetchall();
+ return render_template("lista.html",rekordy = rekordy)
+
+if __name__ == "__main__":
+    app.run()
